9e5dc23b205c0e27c0e3d51956d005786629ae50
Sabrinashh <110469877+Sabrinashh@users.noreply.github.com>
2023-03-17 22:02:30 +0400

    .

---
+++ ColumbiaProject/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ColumbiaProject.Models;
+
+namespace ColumbiaProject.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public List<Order> Orders { get; set; } = new List<Order>();
+        public MemberUpdateViewModel MemberUpdateViewModel { get; set; }
+    }
+}
